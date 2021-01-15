8420b16a3552686e9aa886a1e0ceb6ef1ad0f9a9
Yu-peng Fu <yfu@z3r6p5.42lyon.fr>
2021-01-15 17:36:33 +0100

    test

--- test/main.c
+++ test/main.c
@@ -1,4 +1,9 @@
-#include "get_next_line.h"
+#ifndef BONUS
+#include "../mandatory/get_next_line.h"
+#endif
+#ifdef BONUS
+#include "../bonus/get_next_line_bonus.h"
+#endif
 #include <stdio.h>
 #include <fcntl.h>
 
@@ -12,13 +17,13 @@ int main()
 		//scanf("%d", &n1);
 		char **ans1 = malloc(1 * sizeof(char*));
 		n1 = get_next_line(fd1, ans1);
-		printf("return value ans output of fd1 : %d %s\n", n1, *ans1);
+		printf("return value and result of fd1 : %d %s\n", n1, *ans1);
 		free(*ans1);
 		free(ans1);
 		//scanf("%d", &n2);
         char **ans2 = malloc(1 * sizeof(char*));
 		n2 = get_next_line(fd2, ans2);
-        printf("return value ans output of fd2 : %d %s\n", n2, *ans2);
+        printf("return value and result of fd2 : %d %s\n", n2, *ans2);
         free(*ans2);
 		free(ans2);
     }while (n1 || n2);
@@ -26,6 +31,11 @@ int main()
     close(fd2);
 	while (1)
 	{
+		char **ans = malloc(1 * sizeof(char*));
+		int n1 = get_next_line(0, ans);
+		printf("return value and result of stdin : %d %s\n", n1, *ans);
+		free(*ans);
+		free(ans);
 	}
 }
 
